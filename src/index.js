d1b7fafb1bc9d52f968ff944610568d9281b2c1a
walkunderrain <39767467+walkunderrain@users.noreply.github.com>
2018-09-17 09:06:11 +0300

    done

--- src/index.js
+++ src/index.js
@@ -1,5 +1,63 @@
 // PLEASE DON'T change function name
-module.exports = function makeExchange(currency) {
-    // Your code goes here!
-    // Return an object containing the minimum number of coins needed to make change
+module.exports =
+function makeExchange(currency) {
+    // var object = {};
+    // var H = object.H;
+    // var Q = object.Q;
+    // var D = object.D;
+    // var N = object.N;
+    // var P = object.P;
+    // var er = object.error;
+    //
+    // var h = currency / 50;
+    // if (currency > 10000){ return er = "You are rich, my friend! We don't have so much coins for exchange"};
+    // if (currency == 0){return {}};
+    // if ( h > 0){ var res1 = currency % 50; return H = h} else {
+    //   var q = res1 / 25;
+    //   if (q > 0){ var res3 = res1 % 25; return Q = q } else {
+    //     var d = res3 / 10;
+    //     if (d > 0){ var res4 = res3 % 10; return D = d} else {
+    //       var n = res4 / 5;
+    //       if (n > 0){ var res5 = res4 % 5; return N = n} else {
+    //         var p = res5 / 1;
+    //         if (p > 0){ return oP = p}
+    //       }
+    //     }
+    //   }
+    // }
+    //
+    // return object;
+
+
+    var errorMessage = "You are rich, my friend! We don't have so much coins for exchange";
+    if (currency > 10000) {
+      return {error: errorMessage};
+    }
+    object = {}
+    if (currency <= 0) {
+      return object;
+    }
+    var h = Math.floor(currency / 50);
+    if (h > 0) {
+      object.H = h;
+    }
+    var q = Math.floor(currency % 50 / 25);
+    if (q > 0) {
+      object.Q = q;
+    }
+    var d = Math.floor(currency % 50 % 25 / 10);
+    if (d > 0) {
+      object.D = d;
+    }
+    var n = Math.floor(currency % 50 % 25 % 10 / 5);
+    if (n > 0) {
+      object.N = n;
+    }
+    var p = Math.floor(currency % 50 % 25 % 10 % 5);
+    if (p > 0) {
+      object.P = p;
+    }
+    return object;
 }
+
+// console.log(makeExchange(0))  ;
